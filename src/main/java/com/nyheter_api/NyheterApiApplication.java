7b51653906c9954e5515311a1138261656a767a7
Hilixx <jhonalizan@gmail.com>
2025-07-31 15:49:05 -0300

    first

---
+++ src/main/java/com/nyheter_api/NyheterApiApplication.java
@@ -0,0 +1,13 @@
+package com.nyheter_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyheterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyheterApiApplication.class, args);
+	}
+
+}
